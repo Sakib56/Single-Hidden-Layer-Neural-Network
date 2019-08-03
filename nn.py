66be71a53eeb7a2fd6e64138a5ed3f9c64a2a063
Sakib56 <35700378+Sakib56@users.noreply.github.com>
2019-08-03 21:03:14 +0100

    comments added to train method

--- nn.py
+++ nn.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class NeuralNetwork():
     def __init__(self, inputs, hiddens, outputs):
         layers = (inputs, hiddens, outputs)
@@ -16,7 +17,7 @@ class NeuralNetwork():
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
 
-    # differentiated activation function 
+    # differentiated activation function
     def diffsigmoid(self, x):
         # no need to run through sigmoid again since x has already been through activation
         return x * (1 - x)
@@ -32,37 +33,53 @@ class NeuralNetwork():
         self.trainingData = np.load(path)
         return self.trainingData
 
-    # backpropagation algorithm 
+    # backpropagation algorithm
     def train(self, learningRate, i):
+        # learningRate = step size in gradient decent, i = row in self.trainingData
+
+        # exctaring "x_train" and "y_train"
         inputMat = self.trainingData['x']
         targetMat = self.trainingData['y']
 
+        # getting weight matrix and bias vector for each layer
         weightsIH = self.weightMatrices[0]
         biasH = self.biasVectors[0]
         weightsHO = self.weightMatrices[1]
         biasO = self.biasVectors[1]
 
+        # generating outputs for each layer (hiddens and outputs)
         inputs = np.reshape(inputMat[i], (2, 1))
         hiddens = self.sigmoid(weightsIH @ inputs + biasH)
         outputs = self.sigmoid(weightsHO @ hiddens + biasO)
         targets = np.reshape(targetMat[i], (1, 1))
 
+        # calculating the output error, targets - outputs
+        # calculating output gradient, m = s(o)*(1 - s(o))
+        # hadamard matrix product used (entrywise product)
+        # calculating output deltas, using prior layer; hiddens
         outputErrors = targets - outputs
-        gradients = self.diffsigmoid(outputs)
-        gradients = (gradients * outputErrors) * learningRate
-        deltaWeightsHO = gradients @ np.transpose(hiddens)
+        outputGradients = self.diffsigmoid(outputs)
+        outputGradients = (outputGradients * outputErrors) * learningRate
+        deltaWeightsHO = outputGradients @ np.transpose(hiddens)
 
+        # adjusting the weights and bias vectors by deltas
         weightsHO += deltaWeightsHO
-        biasO += gradients
+        biasO += outputGradients
 
+        # calculating the hidden error, based on output errors
+        # calculating hidden gradient, m = s(h)*(1 - s(h))
+        # hadamard matrix product used (entrywise product)
+        # calculating hidden deltas, using prior layer; inputs
         hiddenErrors = np.transpose(weightsHO) @ outputErrors
         hiddenGradients = self.diffsigmoid(hiddens)
         hiddenGradients = (hiddenGradients * hiddenErrors) * learningRate
         deltaWeightsIH = hiddenGradients @ np.transpose(inputs)
 
+        # adjusting the weights and bias vectors by deltas
         weightsIH += deltaWeightsIH
         biasH += hiddenGradients
 
+        # updating object's copy of matrices and vectors
         self.weightMatrices[0] = weightsIH
         self.biasVectors[0] = biasH
         self.weightMatrices[1] = weightsHO
